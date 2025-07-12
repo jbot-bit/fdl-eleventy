9a403eb64391fd1fd0c51187dca4d7f6dd9126be
jbot-bit <joshdlees@gmail.com>
2025-07-13 01:31:41 +1000

    fake plugin removed

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,8 @@
-const eleventyPluginCsv = require("@11ty/eleventy-plugin-csv");
-
 module.exports = function (eleventyConfig) {
-  // Use the CSV plugin to reliably load data
-  eleventyConfig.addPlugin(eleventyPluginCsv);
-
   // Filter to find items by slug
   eleventyConfig.addFilter("findBySlug", (collection, slug) => {
     if (!collection) return;
-    return collection.find((item) => item.slug === slug); // Simplified for plugin data
+    return collection.find((item) => item.slug === slug);
   });
 
   // Passthrough static assets and evidence files
